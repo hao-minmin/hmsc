304b6f3284c5f704dbfd0fbb35bf7d615db67597
haoxiaomin <2645627070@qq.com>
2020-04-20 17:27:39 +0800

    自定义指令，自定义服务器

---
+++ manage.py
@@ -0,0 +1,22 @@
+from application import app,manager
+
+# flask_script  提供的服务器
+
+from flask_script import Server
+
+#配置runserve指令   添加指令add_command   主机 端口  开发模式  热更新
+manager.add_command('runserver',Server(host="0.0.0.0",port="6000",use_debugger=True,use_reloader=True))
+
+
+# run起来
+def main():
+    manager.run()
+
+if __name__=="__main__":
+
+    try:
+        import sys
+        sys.exit(main())
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
